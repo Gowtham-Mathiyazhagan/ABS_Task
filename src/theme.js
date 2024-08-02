add0f170958eb86a0c8f9b59438932c18a212457
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-08-02 12:46:48 +0530

    feat : coustom theme implemeted

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#e5e7eb",
+      dark: "#d1d5db",
+      light: "#e5e7eb",
+    },
+    secondary: {
+      main: "#16a34a",
+      yellow: "#f59e0b",
+      blue: "#60a5fa",
+    },
+  },
+});
